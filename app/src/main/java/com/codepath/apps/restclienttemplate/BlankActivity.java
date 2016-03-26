1f63ddb71f0bcd50d7e625c57a3a8dd4adc84729
Hoan Le <thanhhoan.le@2359media.com>
2016-03-26 11:23:26 +0700

    test

--- app/src/main/java/com/codepath/apps/restclienttemplate/BlankActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/BlankActivity.java
@@ -1,9 +1,15 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by hoanle on 3/26/16.
  */
 public class BlankActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
