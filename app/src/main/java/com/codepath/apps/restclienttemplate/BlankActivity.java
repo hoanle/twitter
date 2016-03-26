c405283c45245785519330959dbb7452d06b4d50
Hoan Le <thanhhoan.le@2359media.com>
2016-03-26 11:13:16 +0700

    add a blank activity

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/BlankActivity.java
@@ -0,0 +1,9 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hoanle on 3/26/16.
+ */
+public class BlankActivity extends AppCompatActivity{
+}
